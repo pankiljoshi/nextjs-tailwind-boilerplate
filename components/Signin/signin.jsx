e03130f6245e19bdf0350f9a8e454c699f552acc
Pankil Joshi <pankil@zeroek.in>
2024-07-02 14:04:56 +0530

    sign in and sign up api added

--- components/Signin/signin.jsx
+++ components/Signin/signin.jsx
@@ -1,13 +1,37 @@
-import React from 'react'
+import useJwtAuth from "@/Auth/useJwtAuth";
+import { useRouter } from "next/navigation";
+import React from "react";
+import { useForm } from "react-hook-form";
 
 export default function Signin() {
+  const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const { login } = useJwtAuth();
+  const onSubmit = async (data) => {
+    console.log(data)
+    const res = await login({
+      ...data,
+    });
+    if (res?.status) {
+      console.log(res);
+      router.push('/');
+    }
+    console.log("Email:", res, data);
+  };
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-md">
         <h2 className="text-2xl font-bold mb-5 text-gray-800">Sign In</h2>
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="email"
+            >
               Email
             </label>
             <input
@@ -15,10 +39,20 @@ export default function Signin() {
               id="email"
               type="email"
               placeholder="Email"
+              {...register("email", {
+                required: "email is required",
+                pattern: {
+                  value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                  message: "Invalid email address",
+                },
+              })}
             />
           </div>
           <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="password"
+            >
               Password
             </label>
             <input
@@ -26,6 +60,13 @@ export default function Signin() {
               id="password"
               type="password"
               placeholder="Password"
+              {...register("password", {
+                required: "Password is required",
+                minLength: {
+                  value: 6,
+                  message: "Password must be at least 6 characters long",
+                },
+              })}
             />
           </div>
           <div className="flex items-center justify-between mb-6">
@@ -33,14 +74,17 @@ export default function Signin() {
               <input className="mr-2 leading-tight" type="checkbox" />
               <span className="text-sm">Remember me</span>
             </label>
-            <a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
+            <a
+              className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+              href="#"
+            >
               Forgot Password?
             </a>
           </div>
           <div className="flex items-center justify-between">
             <button
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              type="button"
+              type="submit"
             >
               Sign In
             </button>
@@ -48,5 +92,5 @@ export default function Signin() {
         </form>
       </div>
     </div>
-  )
+  );
 }
--- components/Signup/signup.jsx
+++ components/Signup/signup.jsx
@@ -1,13 +1,34 @@
-import React from 'react'
+import useAuth from "@/Auth/useAuth";
+import useJwtAuth from "@/Auth/useJwtAuth";
+import React from "react";
+import { useForm } from "react-hook-form";
 
 export default function Signup() {
+  const { signUp, isLoading } = useJwtAuth();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = async (data) => {
+    console.log(data)
+    const res = await signUp({ email: data.email, password: data.password });
+    if (res?.status) {
+      console.log(res);
+      router.push('/');
+    }
+    console.log("Email:", res, data);
+  };
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-md">
         <h2 className="text-2xl font-bold mb-5 text-gray-800">Signup</h2>
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="username"
+            >
               Username
             </label>
             <input
@@ -18,7 +39,10 @@ export default function Signup() {
             />
           </div>
           <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="email"
+            >
               Email
             </label>
             <input
@@ -26,10 +50,20 @@ export default function Signup() {
               id="email"
               type="email"
               placeholder="Email"
+              {...register("email", {
+                required: "email is required",
+                pattern: {
+                  value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                  message: "Invalid email address",
+                },
+              })}
             />
           </div>
           <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="password"
+            >
               Password
             </label>
             <input
@@ -37,10 +71,20 @@ export default function Signup() {
               id="password"
               type="password"
               placeholder="Password"
+              {...register("password", {
+                required: "Password is required",
+                minLength: {
+                  value: 6,
+                  message: "Password must be at least 6 characters long",
+                },
+              })}
             />
           </div>
           <div className="mb-6">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="confirm-password">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="confirm-password"
+            >
               Confirm Password
             </label>
             <input
@@ -53,7 +97,7 @@ export default function Signup() {
           <div className="flex items-center justify-between">
             <button
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              type="button"
+              type="submit"
             >
               Sign Up
             </button>
@@ -61,5 +105,5 @@ export default function Signup() {
         </form>
       </div>
     </div>
-  )
+  );
 }
